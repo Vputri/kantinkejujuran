07142fac01a3c32fe8829a366d5f72d2764e740c
Vputri <vikaputriariyanti@gmail.com>
2022-07-09 16:48:51 +0700

    first commit

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -2,6 +2,9 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: 'http://wisata-pekalongan.herokuapp.com/',
+  fetchOptions: {
+  	mode: 'no-cors'
+  },
   cache: new InMemoryCache()
 });
 
